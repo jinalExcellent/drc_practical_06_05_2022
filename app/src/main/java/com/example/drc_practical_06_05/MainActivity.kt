a4d433d4fdc156caa7795126c59d5c3e51cf9ee9
Nafisa <nafisa.bagasarwala@excellentwebworld.in>
2022-05-06 17:41:13 +0530

    Intial commit

---
+++ app/src/main/java/com/example/drc_practical_06_05/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.drc_practical_06_05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
